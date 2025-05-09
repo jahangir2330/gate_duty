af8c592c42737433cb45fe085eadaf3b7840da16
Jahangir Alam <jahangir2330@gmail.com>
2025-05-10 00:17:31 +0300

    Test Page Const Change

--- lib/main.dart
+++ lib/main.dart
@@ -11,28 +11,64 @@ import 'package:gipms/core/routes/route_name.dart';
 import 'package:gipms/core/routes/route_name.dart' as route_generator;
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-// ignore: depend_on_referenced_packages
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarBrightness: Brightness.light,
     systemNavigationBarColor: Colors.black,
   ));
   setupServiceLocator();
-  runApp(const MyApp());
+  runApp(const MyAppWrapper());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyAppWrapper extends StatefulWidget {
+  const MyAppWrapper({super.key});
+
+  @override
+  MyAppWrapperState createState() => MyAppWrapperState();
+}
+
+class MyAppWrapperState extends State<MyAppWrapper> {
+  Locale? _currentLocale;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadLocale();
+  }
+
+  Future<void> _loadLocale() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? savedLocale = prefs.getString('locale');
+    if (savedLocale != null) {
+      setState(() {
+        _currentLocale = Locale(
+            savedLocale.split('_')[0],
+            savedLocale.split('_').length > 1
+                ? savedLocale.split('_')[1]
+                : null);
+      });
+    } else {
+      _currentLocale = const Locale('ar'); // Default to Arabic
+    }
+  }
+
+  Future<void> setLocale(Locale newLocale) async {
+    setState(() {
+      _currentLocale = newLocale;
+    });
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('locale', newLocale.toString());
+  }
 
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
         overlays: [SystemUiOverlay.bottom]);
     return MultiProvider(
-      // Use MultiProvider from the provider package
       providers: [
         BlocProvider<AuthStateCubit>(
           create: (context) => AuthStateCubit()..appStarted(),
@@ -41,12 +77,11 @@ class MyApp extends StatelessWidget {
           create: (context) => ButtonStateCubit(),
         ),
         BlocProvider<UserDisplayCubit>(
-          create: (context) => UserDisplayCubit()
-            ..displayUser(), //  Fetch user data here or in initState() of a parent widget
+          create: (context) => UserDisplayCubit()..displayUser(),
         ),
       ],
       child: MaterialApp(
-        //title: Text(AppLocalizations.of(context)!.helloWorld),
+        locale: _currentLocale,
         title: 'GIPMS',
         theme: AppTheme.lightTheme,
         darkTheme: AppTheme.darkTheme,
@@ -65,8 +100,109 @@ class MyApp extends StatelessWidget {
           Locale('ar'), // Arabic
           Locale('en'), // English
         ],
-        locale: const Locale("ar"),
       ),
     );
   }
 }
+
+// Example Widget to Toggle Locale (You can place this anywhere in your UI)
+class LocaleSwitcher extends StatelessWidget {
+  const LocaleSwitcher({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final appState = context.findAncestorStateOfType<MyAppWrapperState>();
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            appState?.setLocale(const Locale('en'));
+          },
+          child: const Text('English'),
+        ),
+        const SizedBox(width: 16),
+        ElevatedButton(
+          onPressed: () {
+            appState?.setLocale(const Locale('ar'));
+          },
+          child: const Text('Arabic'),
+        ),
+      ],
+    );
+  }
+}
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:gipms/presentation/pages/common/splash_screen.dart';
+// import 'package:flutter/services.dart';
+// import 'package:gipms/presentation/pages/home/bloc/user_display_cubit.dart';
+// import 'package:gipms/service_locator.dart';
+// import 'package:gipms/common/bloc/auth/auth_state_cubit.dart';
+// import 'package:gipms/common/bloc/button/button_state_cubit.dart';
+// import 'package:gipms/core/configs/theme/app_theme.dart';
+// import 'package:gipms/core/routes/route_name.dart';
+// import 'package:gipms/core/routes/route_name.dart' as route_generator;
+// import 'package:flutter_localizations/flutter_localizations.dart';
+// import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+// // ignore: depend_on_referenced_packages
+// import 'package:provider/provider.dart';
+
+// void main() {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+//     statusBarBrightness: Brightness.light,
+//     systemNavigationBarColor: Colors.black,
+//   ));
+//   setupServiceLocator();
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+//         overlays: [SystemUiOverlay.bottom]);
+//     return MultiProvider(
+//       // Use MultiProvider from the provider package
+//       providers: [
+//         BlocProvider<AuthStateCubit>(
+//           create: (context) => AuthStateCubit()..appStarted(),
+//         ),
+//         BlocProvider<ButtonStateCubit>(
+//           create: (context) => ButtonStateCubit(),
+//         ),
+//         BlocProvider<UserDisplayCubit>(
+//           create: (context) => UserDisplayCubit()
+//             ..displayUser(), //  Fetch user data here or in initState() of a parent widget
+//         ),
+//       ],
+//       child: MaterialApp(
+//         //title: Text(AppLocalizations.of(context)!.helloWorld),
+//         title: 'GIPMS',
+//         theme: AppTheme.lightTheme,
+//         darkTheme: AppTheme.darkTheme,
+//         themeMode: ThemeMode.system,
+//         debugShowCheckedModeBanner: false,
+//         initialRoute: RouteName.splash,
+//         onGenerateRoute: route_generator.generateRoute,
+//         home: const SplashScreen(),
+//         localizationsDelegates: const [
+//           AppLocalizations.delegate,
+//           GlobalMaterialLocalizations.delegate,
+//           GlobalWidgetsLocalizations.delegate,
+//           GlobalCupertinoLocalizations.delegate,
+//         ],
+//         supportedLocales: const [
+//           Locale('ar'), // Arabic
+//           Locale('en'), // English
+//         ],
+//         locale: const Locale("ar"),
+//       ),
+//     );
+//   }
+// }
--- lib/presentation/pages/home/pages/home.dart
+++ lib/presentation/pages/home/pages/home.dart
@@ -6,7 +6,7 @@ import 'package:gipms/data/viewmodels/getemployee_req_params.dart';
 import 'package:gipms/domain/entities/user_entity.dart';
 import 'package:gipms/domain/usecases/get_employee_usecase.dart';
 import 'package:gipms/domain/usecases/logout.dart';
-//import 'package:gipms/l10n/app_localizations.dart';
+import 'package:gipms/main.dart';
 import 'package:gipms/presentation/pages/auth/signup.dart';
 import 'package:gipms/presentation/pages/common/app_drawer.dart';
 import 'package:gipms/presentation/pages/gen/employee/scan_qrcode_page.dart';
@@ -18,11 +18,50 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../common/bloc/button/button_state.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:shared_preferences/shared_preferences.dart'; // Import shared_preferences
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
   @override
-  //State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Locale? _currentLocale;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadLocale();
+  }
+
+  Future<void> _loadLocale() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? savedLocale = prefs.getString('locale');
+    if (savedLocale != null) {
+      setState(() {
+        _currentLocale = Locale(
+            savedLocale.split('_')[0],
+            savedLocale.split('_').length > 1
+                ? savedLocale.split('_')[1]
+                : null);
+      });
+    } else {
+      _currentLocale = const Locale('ar'); // Default locale
+    }
+  }
+
+  Future<void> _changeLocale(Locale newLocale) async {
+    setState(() {
+      _currentLocale = newLocale;
+    });
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('locale', newLocale.toString());
+    // Force a rebuild of the MaterialApp to apply the new locale
+    final app = context.findAncestorStateOfType<MyAppWrapperState>();
+    app?.setLocale(newLocale);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +80,7 @@ class HomePage extends StatelessWidget {
       body: MultiBlocProvider(
         providers: [
           BlocProvider(create: (context) => UserDisplayCubit()..displayUser()),
-          //  ButtonStateCubit is now provided in main.dart
+          //  ButtonStateCubit is now provided in main.dart
         ],
         child: Center(
           child: BlocBuilder<UserDisplayCubit, UserDisplayState>(
@@ -53,6 +92,34 @@ class HomePage extends StatelessWidget {
                 return Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: DropdownButtonFormField<Locale>(
+                        value: _currentLocale,
+                        decoration: InputDecoration(
+                          labelText: AppLocalizations.of(context)!.language,
+                          border:
+                              const OutlineInputBorder(), // Added border for better UI
+                        ),
+                        icon: const Icon(Icons.language),
+                        elevation: 16,
+                        onChanged: (Locale? newLocale) {
+                          if (newLocale != null) {
+                            _changeLocale(newLocale);
+                          }
+                        },
+                        items: const [
+                          DropdownMenuItem(
+                            value: Locale('ar'),
+                            child: Text('العربية'),
+                          ),
+                          DropdownMenuItem(
+                            value: Locale('en'),
+                            child: Text('English'),
+                          ),
+                        ],
+                      ),
+                    ),
                     Image.asset(
                       'assets/images/logo-gif-Animate.gif',
                       height: 120,
@@ -125,16 +192,142 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
-
-  Widget _logout(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(32),
-      child: BasicAppButton(
-        title: 'Logout',
-        onPressed: () {
-          context.read<ButtonStateCubit>().excute(usecase: sl<LogoutUseCase>());
-        },
-      ),
-    );
-  }
 }
+// // ignore_for_file: unused_import
+// import 'package:gipms/common/bloc/button/button_state_cubit.dart';
+// import 'package:gipms/common/widgets/button/basic_app_button.dart';
+// import 'package:gipms/core/routes/route_name.dart';
+// import 'package:gipms/data/viewmodels/getemployee_req_params.dart';
+// import 'package:gipms/domain/entities/user_entity.dart';
+// import 'package:gipms/domain/usecases/get_employee_usecase.dart';
+// import 'package:gipms/domain/usecases/logout.dart';
+// //import 'package:gipms/l10n/app_localizations.dart';
+// import 'package:gipms/presentation/pages/auth/signup.dart';
+// import 'package:gipms/presentation/pages/common/app_drawer.dart';
+// import 'package:gipms/presentation/pages/gen/employee/scan_qrcode_page.dart';
+// import 'package:gipms/presentation/pages/home/bloc/user_display_cubit.dart';
+// import 'package:gipms/presentation/pages/home/bloc/user_display_state.dart';
+// import 'package:gipms/service_locator.dart';
+// import 'package:flutter/gestures.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import '../../../../common/bloc/button/button_state.dart';
+// import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+// class HomePage extends StatelessWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(AppLocalizations.of(context)!.helloWorld),
+//         automaticallyImplyLeading: false,
+//         leading: Builder(
+//           builder: (context) => IconButton(
+//             icon: const Icon(Icons.menu),
+//             onPressed: () => Scaffold.of(context).openDrawer(),
+//           ),
+//         ),
+//       ),
+//       drawer: const AppDrawer(),
+//       body: MultiBlocProvider(
+//         providers: [
+//           BlocProvider(create: (context) => UserDisplayCubit()..displayUser()),
+//           //  ButtonStateCubit is now provided in main.dart
+//         ],
+//         child: Center(
+//           child: BlocBuilder<UserDisplayCubit, UserDisplayState>(
+//             builder: (context, state) {
+//               if (state is UserLoading) {
+//                 return const CircularProgressIndicator();
+//               }
+//               if (state is UserLoaded) {
+//                 return Column(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: [
+//                     Image.asset(
+//                       'assets/images/logo-gif-Animate.gif',
+//                       height: 120,
+//                       width: 120,
+//                       fit: BoxFit.cover,
+//                       errorBuilder: (context, error, stackTrace) =>
+//                           const Icon(Icons.error, size: 120),
+//                     ),
+//                     _username(state.userEntity),
+//                     const SizedBox(height: 10),
+//                     _rolename(state.userEntity),
+//                     const SizedBox(height: 10),
+//                     _gatename(state.userEntity),
+//                     _qrscan(context),
+//                     const SizedBox(height: 10),
+//                     // _logout(context),
+//                   ],
+//                 );
+//               }
+//               if (state is LoadUserFailure) {
+//                 return Text(state.errorMessage);
+//               }
+//               return Container();
+//             },
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+//   Widget _username(UserEntity user) {
+//     return Text.rich(
+//       TextSpan(children: [
+//         const TextSpan(
+//           text: "Welcome ",
+//           style:
+//               TextStyle(color: Color(0xff3B4054), fontWeight: FontWeight.w500),
+//         ),
+//         TextSpan(
+//           text: user.username,
+//           style: const TextStyle(
+//               color: Color(0xff3461FD), fontWeight: FontWeight.w500),
+//         )
+//       ]),
+//     );
+//   }
+
+//   Widget _gatename(UserEntity user) {
+//     return Text(
+//       user.gatenamear,
+//       style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 19),
+//     );
+//   }
+
+//   Widget _rolename(UserEntity user) {
+//     return Text(
+//       user.rolename,
+//       style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 19),
+//     );
+//   }
+
+//   Widget _qrscan(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.all(32),
+//       child: BasicAppButton(
+//         title: 'Scan QR Code',
+//         onPressed: () {
+//           Navigator.of(context).pushReplacementNamed(RouteName.qrscan);
+//         },
+//       ),
+//     );
+//   }
+
+//   Widget _logout(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.all(32),
+//       child: BasicAppButton(
+//         title: 'Logout',
+//         onPressed: () {
+//           context.read<ButtonStateCubit>().excute(usecase: sl<LogoutUseCase>());
+//         },
+//       ),
+//     );
+//   }
+// }
